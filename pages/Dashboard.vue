a6f9bd9cade50f870e22c18dd85b8447c3c0b400
Timi <oblacishere@gmail.com>
2024-07-03 21:23:26 +0100

    chore: add dashboard page

---
+++ pages/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+	<header>
+		<template v-if="loggedIn">
+			<p>
+				Hi
+				{{ user?.username }}
+			</p>
+			<p @click="logout">Log out</p>
+		</template>
+		<p v-else>Hi Guest, <nuxt-link :to="{ name: 'login' }">Login</nuxt-link></p>
+	</header>
+</template>
+
+<script setup>
+const { loggedIn, user, clear } = useUserSession();
+
+const logout = () => {
+	clear();
+};
+</script>
+
+<style lang="scss" scoped></style>
