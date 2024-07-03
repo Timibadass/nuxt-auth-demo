bac7c1cd62695d20c1a36c7ecb945a68da405007
Timi <oblacishere@gmail.com>
2024-07-03 21:23:45 +0100

    chore: add signup page

---
+++ pages/signup.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="signup">
+		<AuthForm :loading="loading" @submit="register" title="Sign up" />
+		<p class="signup__text">
+			Already registered?
+			<nuxt-link :to="{ name: 'login' }">Log in</nuxt-link>
+		</p>
+	</div>
+</template>
+
+<script setup>
+import AuthForm from "@/components/AuthForm.vue";
+const loading = ref(false);
+
+
+const register = async (body) => {
+	loading.value = true;
+	try {
+		await $fetch("/api/auth/signup", {
+			method: "POST",
+			body,
+		});
+		window.location.href = "/dashboard";
+		loading.value = false;
+	} catch (error) {
+		alert(error.statusMessage);
+		loading.value = false;
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.signup {
+	width: 100%;
+	padding: 50px;
+	max-width: 400px;
+	margin: auto;
+	color: #333333;
+  &__text {
+    text-align: right;
+    & a {
+      text-decoration: underline;
+      color: inherit;
+    }
+  }
+}
+</style>
